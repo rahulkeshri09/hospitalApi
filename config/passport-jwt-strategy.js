5c0a6540457fa8cddaafb474b54eef61e68f6fbf
rahul <rahulkeshri09.rk@gmail.com>
2020-07-27 01:20:13 +0530

    configure the passport-jwt method

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,22 @@
+const passport=require('passport');
+const jwtStrategy=require('passport-jwt').Strategy;
+const ExtractJWT=require('passport-jwt').ExtractJwt;
+const Doctor=require('../models/doctor');
+let opts={
+    jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey:'hospital'
+}
+passport.use(new jwtStrategy(opts,function(jwtPayload,done){
+    Doctor.findById(jwtPayload._id,function(err,doctor){
+        if(err){
+            console.log('error in finding user from jwt',err);
+            return
+        }
+        if(doctor){
+            return done(null,doctor);
+        }else{
+            return done(null,false);
+        }
+    });
+}));
+module.exports=passport;
