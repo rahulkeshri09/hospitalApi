6397002fa943de23faef3e2bb32ca6cf90f6676c
rahul <rahulkeshri09.rk@gmail.com>
2020-07-27 00:54:14 +0530

    creating mongoDb Database

---
+++ config/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/hospitalApi_development');
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,"error in connecting to mongoDB"));
+db.once('open',function(){
+    console.log("connected to database :: mongoDB");
+})
+module.exports=db;
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const app=express();
 const port=8000;
-
+const db=require('./config/mongoose');
+app.use(express.urlencoded());
+//entry point of each routes
+app.use('/',require('./routes'));
+// starting the server
 app.listen(port,function(err){
     if(err){
         console.log(`error in starting the server:${err}`);
